cff432790ca5f69ad94f6ae41d33385808bd86cf
Antonio S. Cofiño <cofinoa@gmail.com>
2026-06-02 13:53:25 +0200

    Refactor build.sh for better readability and add error handling; remove unnecessary blank line in meta.yaml

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 export DISABLE_AUTOBREW=1
-conda-recipe-upstream-validator --description $SRC_DIR/DESCRIPTION --meta-yaml recipe/meta.yaml
+conda-recipe-upstream-validator \
+  --description "$SRC_DIR/DESCRIPTION" \
+  --meta-yaml recipe/meta.yaml || exit 1
 ${R} CMD INSTALL --build . ${R_ARGS}
